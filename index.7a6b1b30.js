c2d58f3c97ba1290215c036aae5e258979cb7ad6
al-busova <96522313+al-busova@users.noreply.github.com>
2022-08-14 20:59:52 +0000

    Deploying to gh-pages from @ al-busova/goit-js-hw-10@e62dd28a43369fd9e55b48e0d8da933f4603886d 🚀

---
+++ index.7a6b1b30.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/all").then((o=>o.json())).then((o=>console.log(o))).catch((o=>{console.log(o)}));
+//# sourceMappingURL=index.7a6b1b30.js.map
---
+++ index.b6d17690.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/all").then((function(n){return n.json()})).then((function(n){return console.log(n)})).catch((function(n){console.log(n)}));
+//# sourceMappingURL=index.b6d17690.js.map
