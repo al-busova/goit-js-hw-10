2984de0d1529f861fd3207ad591adfa79d108ce5
Alena Busova <96522313+al-busova@users.noreply.github.com>
2022-08-16 11:52:06 +0300

    bugfix

--- src/index.js
+++ src/index.js
@@ -2,76 +2,71 @@ import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-
 const DEBOUNCE_DELAY = 300;
 const searchCountryEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
-
 searchCountryEl.addEventListener('input', debounce(getInput, DEBOUNCE_DELAY));
-
 function getInput(e) {
-    if (e.target.value.trim() === '') {
-        resetPage();
+  if (e.target.value.trim() === '') {
+    resetPage();
+    return;
+  }
+  fetchCountries(e.target.value.trim())
+    .then(data => {
+      if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
         return;
-    }
-    fetchCountries(e.target.value.trim())
-        .then(data => {
-        // if (data.length > 10) {
-        //     Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-        //     return;
-        // }
-        // resetPage();
-            console.dir(data);
-        // insertContent(data);
-        })
-        .catch(() => Notiflix.Notify.failure("Oops, there is no country with that name")); 
+      }
+      resetPage();
+      insertContent(data);
+    })
+    .catch(() =>
+      Notiflix.Notify.failure('Oops, there is no country with that name')
+    );
 }
-     
 function createInfoCountry(countries) {
-    const content = [];
-
-    for (country of countries) {
-        const item = `
-        <ul >
-        <li class = "country-list_item dec-item">
-        <img src= "${country.flags.svg}" alt = 'Flag of ${country.name}' width = 30>
-        <h2>${country.name}</h2></li>
-        <li class = "dec-item"><p ><span class = "country-dec">Capital: </span>${country.capital}</p></li>
-        <li class = "dec-item"><p ><span class = "country-dec">Population: </span>${country.population}</p></li>
-        <li class = "dec-item"><p ><span class = "country-dec">Languages: </span>
-        ${country.languages.map(language => ` ${language.name}`)}</p></li>
-       </ul>`
-        content.push(item);
-    }
-
-    res = content.join('');
-    return res;
+  return countries
+    .map(
+      country => `
+    <div class="country-list_item dec-item">
+    <img src= "${country.flags.svg}" alt='Flag of ${country.name}' width="30">
+    <h2>${country.name}</h2></li>
+    <div class="dec-item"><p ><span class="country-dec">Capital: </span>${
+      country.capital
+    }</p></div>
+    <div class="dec-item"><p ><span class="country-dec">Population: </span>${
+      country.population
+    }</p></div>
+    <div class="dec-item"><p><span class="country-dec">Languages: </span>
+    ${country.languages.map(language => ` ${language.name}`)}</p></div>
+    </div>
+  `
+    )
+    .join('');
 }
-    
 function createListCountries(countries) {
-    const content = [];
-
-       for (country of countries) {
-           const item = `
-        <li class = "country-list_item"><img src= "${country.flags.svg}" alt = 'Flag of ${country.name}' width = 30>
-        <p>${country.name}</p></li>`
-        content.push(item);
-       }
-    
-    res = content.join('');
-    return res;
+  return countries
+    .map(
+      country => `
+  <li class = "country-list_item"><img src= "${country.flags.svg}" alt = 'Flag of ${country.name}' width = 30>
+  <p>${country.name}</p></li>`
+    )
+    .join('');
 }
-
 function insertContent(countries) {
-    if (countries.length === 1) {
-        countryInfoEl.insertAdjacentHTML('beforeend', createInfoCountry(countries));
-    } else {
-        countryListEl.insertAdjacentHTML('beforeend', createListCountries(countries));
-    }
+  if (countries.length === 1) {
+    countryInfoEl.insertAdjacentHTML('beforeend', createInfoCountry(countries));
+  } else {
+    countryListEl.insertAdjacentHTML(
+      'beforeend',
+      createListCountries(countries)
+    );
+  }
 }
-
 function resetPage() {
-        countryListEl.innerHTML = '';
-        countryInfoEl.innerHTML = '';
+  countryListEl.innerHTML = '';
+  countryInfoEl.innerHTML = '';
 }
